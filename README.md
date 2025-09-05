2e023fe999eddc804cc9e3d71da3acd2bd6712b1
Dorion de Gobeo <ddegobeo@gmail.com>
2025-09-05 09:55:57 -0400

    Move relevant comments to README

--- README.md
+++ README.md
@@ -1,13 +1,104 @@
-Here is a webpage that shows various visualizations of operations on a rubics cube. 
+A set of functions for intitiating and manipulating a 3x3x3 Rubik's Cube.
 
-Interesting Questions:
+DEFINITIONS
 
-What is the simplest set of operations that traverses all solutions of the rubics cube? 
-    - I feel like certain operations just make an infinite loop that traverses only a subset of the solutions
-Given a solved cube, how many operations would i take to eventually come back to the original solution?
-What is the furthest away you can be from the solution?>
+AXES
+0 FB Front Back axis
+1 UD Up Down axis
+2 LR Left Right axis
 
-Observations
-    - The center tile of each face has an opposite color that remains fixed.
-        - White is opposite Yellow, Red opposite Orange and Green is opposite Blue
+ELEMENTS
+there are 3 x 3 x 3 = 27 elements in a rubik's cube and each element has 6
+faces. Each element is specified as 1x6 array with a number indicating color
+and position in the array indicating orientation:
+Let the starting colors be in the given orientation:
+
+0 u Up (White)
+1 f Forward (Red)
+2 l Left (Green)
+3 b Back (Orange)
+4 r Right (Blue)
+5 d Down (Yellow)
+
+[u, f, l, b, r, d]
+
+CUBE DEFINITION
+a cube may be represented as a 3x3x3x6 matrix because each of the 27 elements
+of a Rubik's cube has 6 faces.
+Now, imagine taking 3x3x1 vertical slices of the cube looking at the red face
+with white facing up. Then, on the first slice, start at the top left and
+read the elements from left to right downwards. Then, move on to the next
+slice.
+A number has been assigned to each visible face of an element.
+Up/White: 0-9
+Forward/Red: 10-18
+Left/Green: 19-27
+Back/Orange: 28-36
+Right/Blue: 37-45
+Down/Yellow: 46-54
+CREATING AXIS MAPS
+element definition: [u, f, l, b, r, d]
+
+
+MAPPING COLORS TO NEW ORIENTATION AFTER ROTATION
+on the FB axis, Clockwise (CW) rotation goes like this
+l -> u-> 2
+f -> f-> 1
+d -> l-> 5
+b -> b-> 3
+u -> r-> 0
+r -> d-> 4
+
+CW on FB: [2, 1, 5, 3, 0, 4]
+
+on the FB axis, Counter Clockwise (CCW) rotation goes like this
+r -> u -> 4
+f -> f -> 1
+u -> l -> 0
+b -> b -> 3
+d -> r -> 4
+l -> d -> 2
+
+CCW on FB: [4, 1, 0, 3, 5, 2]
+
+on the UD axis, CW rotation goes like this
+u -> u -> 0
+r -> f -> 4
+f -> l -> 1
+l -> b -> 2
+b -> r -> 3
+d -> d -> 5
+
+CW on UD: [0, 4, 1, 2, 3, 5]
+
+on the UD axis, CCW rotation goes like this
+u -> u -> 0
+l -> f -> 2
+b -> l -> 3
+r -> b -> 4
+f -> r -> 1
+d -> d -> 5
+
+CCW on UD: [0, 2, 3, 4, 1, 5]
+
+on the LR axis, CW rotation goes like this
+b -> u -> 3
+u -> f -> 0
+l -> l -> 2
+d -> b -> 5
+r -> r -> 4
+f -> d -> 1
+
+CW on LR: [3, 0, 2, 5, 4, 1]
+
+on the LR axis, CCW rotation goes like this
+f -> u -> 1
+d -> f -> 5
+l -> l -> 2
+u -> b -> 0
+r -> r -> 4
+b -> d -> 3
+
+CCW on LR: [1, 5, 2, 0, 4, 3]
+
 
--- rubik.py
+++ rubik.py
@@ -1,126 +1,6 @@
 """A bunch of utility functions for manipulating a Rubik's cube"""
-import numpy as np
 from random import randrange
-
-# DEFINITIONS
-
-# AXES
-# 0 FB Front Back axis
-# 1 UD Up Down axis
-# 2 LR Left Right axis
-
-# ELEMENTS
-# there are 3 x 3 x 3 = 27 elements in a rubik's cube and each element has 6
-# faces. Each element is specified as 1x6 array with a number indicating color
-# and position in the array indicating orientation:
-# Let the starting colors be in the given orientation:
-
-# 0 u Up (White)
-# 1 f Forward (Red)
-# 2 l Left (Green)
-# 3 b Back (Orange)
-# 4 r Right (Blue)
-# 5 d Down (Yellow)
-
-# [u, f, l, b, r, d]
-
-# CUBE DEFINITION
-# a cube may be represented as a 3x3x3x6 matrix because each of the 27 elements
-# of a Rubik's cube has 6 faces.
-# Now, imagine taking 3x3x1 vertical slices of the cube looking at the red face
-# with white facing up. Then, on the first slice, start at the top left and
-# read the elements from left to right downwards. Then, move on to the next
-# slice.
-# A number has been assigned to each visible face of an element.
-# Up/White: 0-9
-# Forward/Red: 10-18
-# Left/Green: 19-27
-# Back/Orange: 28-36
-# Right/Blue: 37-45
-# Down/Yellow: 46-54
-
-
-# MAPPING COLORS TO NEW ORIENTATION
-# element definition: [u, f, l, b, r, d]
-# on the FB axis, Clockwise (CW) rotation goes like this
-# l -> u-> 2
-# f -> f-> 1
-# d -> l-> 5
-# b -> b-> 3
-# u -> r-> 0
-# r -> d-> 4
-
-# CW on FB: [2, 1, 5, 3, 0, 4]
-
-# on the FB axis, Counter Clockwise (CCW) rotation goes like this
-# r -> u -> 4
-# f -> f -> 1
-# u -> l -> 0
-# b -> b -> 3
-# d -> r -> 4
-# l -> d -> 2
-
-# CCW on FB: [4, 1, 0, 3, 5, 2]
-
-# on the UD axis, CW rotation goes like this
-# u -> u -> 0
-# r -> f -> 4
-# f -> l -> 1
-# l -> b -> 2
-# b -> r -> 3
-# d -> d -> 5
-
-# CW on UD: [0, 4, 1, 2, 3, 5]
-
-# on the UD axis, CCW rotation goes like this
-# u -> u -> 0
-# l -> f -> 2
-# b -> l -> 3
-# r -> b -> 4
-# f -> r -> 1
-# d -> d -> 5
-
-# CCW on UD: [0, 2, 3, 4, 1, 5]
-
-
-# on the LR axis, CW rotation goes like this
-# b -> u -> 3
-# u -> f -> 0
-# l -> l -> 2
-# d -> b -> 5
-# r -> r -> 4
-# f -> d -> 1
-
-# CW on LR: [3, 0, 2, 5, 4, 1]
-
-
-# on the LR axis, CCW rotation goes like this
-# f -> u -> 1
-# d -> f -> 5
-# l -> l -> 2
-# u -> b -> 0
-# r -> r -> 4
-# b -> d -> 3
-
-# CCW on LR: [1, 5, 2, 0, 4, 3]
-
-# SLICING
-# FB 90deg rotation
-# rotate(0, 0) -> cube[0, :, :]
-# rotate(0, 1) -> cube[1, :, :]
-# rotate(0, 2) -> cube[2, :, :]
-
-# UD 90deg rotation
-# rotate(1, 0) -> cube[:, 0, :]
-# rotate(1, 1) -> cube[:, 1, :]
-# rotate(1, 2) -> cube[:, 2, :]
-
-# LR 90deg rotation
-# rotate(2, 0) -> cube[:, :, 0]
-# rotate(2, 1) -> cube[:, :, 1]
-# rotate(2, 2) -> cube[:, :, 2]
-
-solved_faces = np.split(np.arange(1, 55), 6)
+import numpy as np
 
 face_selectors = [
     (slice(None), 0, slice(None), 0),
@@ -130,6 +10,8 @@ face_selectors = [
     (slice(None), slice(None), 2, 4),
     (slice(None), 2, slice(None), 5)
 ]
+
+solved_faces = np.split(np.arange(1, 55), 6)
 
 
 def randomize_cube(cube, rotations=30):
@@ -190,7 +72,7 @@ def rotate_slice(elements, axis=0, direction="CW"):
 
 
 def get_axis_map(axis=0, direction="CW"):
-
+    '''gets the map for mapping colors to their new orientation'''
     return {
         "CW": [
             [2, 1, 5, 3, 0, 4],
